2b6838cab39311e847938582ad39d3333058c0b3
ViktorVan22 <zcg5368@163.com>
2022-07-13 18:57:17 +0800

    add eslint and other things

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,9 @@
 /**
  * @description 程序主入口
  */
+const say = (statements: string) => {
+  console.log(statements);
+};
+const a = 3;
+
+say("Tecvan");
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,20 @@
 const path = require("path");
+const ESLintPlugin = require("eslint-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: {
     index: "./src/index.tsx",
   },
+  devtool: false,
   output: {
     filename: "index.js",
     path: path.resolve(__dirname, "./dist"),
     library: "zenz-design",
     libraryTarget: "umd",
   },
+  // 添加 eslint-webpack-plugin 插件实例
+  plugins: [new ESLintPlugin({ extensions: [".ts", ".js", ".tsx", ".jsx"] })],
   module: {
     // module属性用于声明模块处理规则
     rules: [
@@ -29,7 +33,10 @@ module.exports = {
         rules: [
           {
             test: /\.tsx?$/,
-            use: "ts-loader",
+            use: {
+              loader: "babel-loader",
+              options: { presets: ["@babel/preset-typescript"] },
+            },
           },
         ],
       },
