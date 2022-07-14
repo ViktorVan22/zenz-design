d3120b35d04a381d885721b9fdb2c39401de43c8
ViktorVan22 <zcg5368@163.com>
2022-07-14 15:47:57 +0800

    add alias to import absolute path

--- src/zenz_container/ProjectLayout.tsx
+++ src/zenz_container/ProjectLayout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, Route, Routes } from "react-router-dom";
-import { ButtonExample } from "button/button.example";
-import { IconExample } from "icon/icon.example";
+import { ButtonExample } from "@button/button.example";
+import { IconExample } from "@icon/icon.example";
 import { NoMatch } from "./NoMatch";
 
 const ProjectLayout = () => {
--- webpack.config.js
+++ webpack.config.js
@@ -56,8 +56,8 @@ module.exports = {
   resolve: {
     extensions: [".ts", ".js", ".tsx", ".jsx"],
     alias: {
-      button: path.resolve(__dirname, "./src/button"),
-      icon: path.resolve(__dirname, "./src/icon"),
+      "@button": path.resolve(__dirname, "src/button"),
+      "@icon": path.resolve(__dirname, "src/icon"),
     },
   },
 };
