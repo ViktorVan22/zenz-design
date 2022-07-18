4f0da06d8051ec1151f7d5c42142195f97e54fe4
ViktorVan22 <zcg5368@163.com>
2022-07-18 19:59:09 +0800

    优化carousel样式

--- src/carousel/carousel.example.tsx
+++ src/carousel/carousel.example.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Carousel } from "./carousel";
+import { CarouselCopy } from "./carouselCopy";
 
 const contentStyle: React.CSSProperties = {
   height: "160px",
@@ -12,10 +13,8 @@ const contentStyle: React.CSSProperties = {
 const CarouselExample = () => {
   return (
     <div className="demo-container">
+      <h2>常规轮播图</h2>
       <Carousel className="carousel-demo">
-        <span>
-          <h3 style={contentStyle}>carousel span</h3>
-        </span>
         <div>
           <h3 style={contentStyle}>1</h3>
         </div>
@@ -29,6 +28,24 @@ const CarouselExample = () => {
           <h3 style={contentStyle}>4</h3>
         </div>
       </Carousel>
+      <h2>抄来的滑动轮播图</h2>
+      <CarouselCopy>
+        <li>
+          <h3 style={contentStyle}>1</h3>
+        </li>
+        <li>
+          <h3 style={contentStyle}>2</h3>
+        </li>
+        <li>
+          <h3 style={contentStyle}>3</h3>
+        </li>
+        <li>
+          <h3 style={contentStyle}>4</h3>
+        </li>
+        <li>
+          <h3 style={contentStyle}>5</h3>
+        </li>
+      </CarouselCopy>
     </div>
   );
 };
